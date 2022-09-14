b2b9557069579842252882834ba90fa8871161e1
morganwall <morgan@mlwall.net>
2022-09-14 12:51:05 +0100

    Update dllmain.cpp
    Capitalized the signatures, no real reason to do so, just me being stupid :)
    Also added a check to make sure that the file "f" was actually created before we try to close it. Also no real point to this, just wanted to get rid of the warning in Visual Studio.

--- War Thunder Offset Dumper/src/dllmain.cpp
+++ War Thunder Offset Dumper/src/dllmain.cpp
@@ -88,9 +88,9 @@ DWORD WINAPI MainThread(HMODULE hModule)
     cout << "War Thunder Offset Dumper by morganlewiswall.\n\n";
 	
     uintptr_t moduleBase = (uintptr_t)GetModuleHandle("aces.exe");
-    uintptr_t cGame = (GetOffset<std::uintptr_t>(get("48 8b 0d ? ? ? ? 48 8b 01 ff 50 ? 48 85 ed 74 ? 0f 31"), 0x3));
-    uintptr_t cLocalPlayer = (GetOffset<std::uintptr_t>(get("48 8b 15 ? ? ? ? 48 85 d2 74 ? 41 ba ? ? ? ? f6 82 ? ? ? ? ? 75"), 0x3));
-    uintptr_t cPlayerList = (GetOffset<std::uintptr_t>(get("48 8b 0d ? ? ? ? 89 c0 48 8b 1c c1 48 85 db 74 ? 48 8b 05"), 0x3));
+    uintptr_t cGame = (GetOffset<std::uintptr_t>(get("48 8D 0D ? ? ? ? 48 8B 01 FF 50 ? 48 85 ED 74 ? 0F 31"), 0x3));
+    uintptr_t cLocalPlayer = (GetOffset<std::uintptr_t>(get("48 8B 15 ? ? ? ? 48 85 D2 74 ? 41 BA ? ? ? ? F6 82 ? ? ? ? ? 75"), 0x3));
+    uintptr_t cPlayerList = (GetOffset<std::uintptr_t>(get("48 8B 0D ? ? ? ? 89 C0 48 8B 1C C1 48 85 DB 74 ? 48 8B 05"), 0x3));
 
     cout << "Addresses:\n";
     cout << "Base:         0x" << std::hex << std::uppercase << moduleBase << "\n";
@@ -112,7 +112,8 @@ DWORD WINAPI MainThread(HMODULE hModule)
         if (GetAsyncKeyState(VK_DELETE))
             break;
 
-    fclose(f);
+    if (f)
+        fclose(f);
     FreeConsole();
     FreeLibraryAndExitThread(hModule, 0);
     return 0;
